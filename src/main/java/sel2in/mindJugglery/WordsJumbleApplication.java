f4c5892d9d67ef49ce6f17dfe31cc289bdc0be51
Tushar <tushar.kapila@accoliteindia.com>
2021-07-11 16:15:53 +0530

    other branches

--- src/main/java/sel2in/mindJugglery/WordsJumbleApplication.java
+++ src/main/java/sel2in/mindJugglery/WordsJumbleApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//import io.swagger.oas.inflector.config.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 @EnableEurekaClient
 @SpringBootApplication
 public class WordsJumbleApplication {
@@ -15,6 +24,34 @@ public class WordsJumbleApplication {
 
 	private static String getVersion() {
 		return "1.0.111";
+	}
+
+//	@Bean
+//	Configuration configuration(ApplicationContext applicationContext)
+//	{
+//		Configuration configuration = Configuration.read();
+//		configuration.setControllerFactory((cls, operation) -> applicationContext.getBean(cls));
+//		return configuration;
+//	}
+
+	/**
+	 * Since we're using both Actuator and Jersey, we need to use Springs
+	 * <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html#_filter_based_cors_support">Filter based CORS support</a>
+	 *
+	 * @return corsFilter
+	 */
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 }
